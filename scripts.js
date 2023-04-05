c6bb61e1d66d09b7b68ce58e021534067bcd73d1
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-05 13:41:08 -0600

    Debug determineWinner, adjust styling, correct score display

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Create score variables to store scores
 let userScore = 0;
 let computerScore = 0;
 let playerSelection = "";
@@ -6,9 +5,90 @@ let computerSelection = "";
 const options = ["Rock", "Paper", "Scissors"];
 const playerSelectionPlaceholder = document.getElementById("player-selection-result");
 const computerSelectionPlaceholder = document.getElementById("computer-selection-result");
-const userScorePlaceholder = document.getElementById("player-score");
-const computerScorePlaceholder = document.getElementById("computer-score");
+const userScorePlaceholder = document.getElementById("user-score-count");
+const computerScorePlaceholder = document.getElementById("computer-score-count");
 const endGameMessage = document.getElementById("end-game-message");
+
+function getComputerChoice () {
+    const randomIndex = Math.floor(Math.random() * options.length)
+    const randomOption = options[randomIndex]
+    computerSelection = randomOption;
+}
+
+function determineWinner() {
+    if (playerSelection === computerSelection) {
+        userScore++
+        computerScore++
+    }
+    else if (
+        (playerSelection === "Rock" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Rock") 
+        ){
+        computerScore++
+    } else {
+        userScore++
+    }
+}
+
+function displayUpdatedContent() {
+    disableButtons();
+    computerSelectionPlaceholder.textContent = "";
+    playerSelectionPlaceholder.textContent = playerSelection;
+    setTimeout(function() {
+        computerSelectionPlaceholder.textContent = computerSelection;
+    }, 3000);
+    setTimeout(function() {
+        userScorePlaceholder.textContent = userScore.toString();
+        computerScorePlaceholder.textContent = computerScore.toString();
+        enableButtons();
+    }, 5000);
+    ;
+}
+
+function checkEndGame() {
+    setTimeout(function() {
+        if (userScore === 5 && computerScore === 5) {
+            disableButtons();
+            endGameMessage.style.color = "black";
+            endGameMessage.textContent = "It's a tie!";
+        }
+        else if (userScore === 5) {
+            disableButtons();
+            endGameMessage.style.color = "green";
+            endGameMessage.textContent = "You win! Congratulations!";  
+        }
+        else if (computerScore === 5) {
+            disableButtons();
+            endGameMessage.style.color = "red"
+            endGameMessage.textContent = "You've lost. Try again!";
+        }
+    }, 6000);
+}
+
+function reset() {
+    userScore = 0;
+    computerScore = 0;
+    playerSelectionPlaceholder.textContent = "";
+    computerSelectionPlaceholder.textContent = "";
+    userScorePlaceholder.textContent = "";
+    computerScorePlaceholder.textContent = "";
+    endGameMessage.textContent = "";
+}
+
+function disableButtons() {
+    const buttons = document.querySelectorAll('.selection');
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
+function enableButtons() {
+    const buttons = document.querySelectorAll('.selection');
+    buttons.forEach(button => {
+        button.disabled = false;
+    });
+}
 
 // Allow user to select either rock paper scissors
 document.getElementById("rockButton").addEventListener("click", function() {
@@ -35,61 +115,6 @@ document.getElementById("scissorsButton").addEventListener("click", function() {
     checkEndGame();
 });
 
-document.getElementById("reset-button").addEventListener("click", reset());
+document.getElementById("reset-button").addEventListener("click", reset);
 
-function getComputerChoice () {
-    const randomIndex = Math.floor(Math.random() * options.length)
-    const randomOption = options[randomIndex]
-    computerSelection = randomOption;
-}
 
-function determineWinner() {
-    if (playerSelection === "Rock" && computerSelection === "Rock") {
-        userScore++
-        computerScore++
-    }
-    else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        computerScore++
-    }
-    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        userScore++
-    }
-}
-
-function displayUpdatedContent() {
-    computerSelectionPlaceholder.textContent = "";
-    playerSelectionPlaceholder.textContent = playerSelection;
-    setTimeout(function() {
-        computerSelectionPlaceholder.textContent = computerSelection;
-    }, 3000);
-    setTimeout(function() {
-        userScorePlaceholder.textContent += userScore.toString();
-        computerScorePlaceholder.textContent += computerScore.toString();
-    }, 5000);
-}
-
-function checkEndGame() {
-    setTimeout(function() {
-        if (userScore === 5 && computerScore === 5) {
-            endGameMessage.textContent = "It's a tie!";
-        }
-        else if (userScore === 5) {
-            endGameMessage.textContent = "You win! Congratulations!";  
-        }
-        else if (computerScore === 5) {
-            endGameMessage.textContent = "You've lost. Try again!";
-        }
-    }, 6000);
-}
-
-function reset() {
-    userScore = 0;
-    computerScore = 0;
-    playerSelectionPlaceholder.textContent = "";
-    computerSelectionPlaceholder.textContent = "";
-    userScorePlaceholder.textContent = "";
-    computerScorePlaceholder.textContent = "";
-}
-
-// Display a message declaring the winner with current score
-// Display final message of winner after 5 rounds
