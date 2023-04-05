17ea302195e745f02892070fb2ec46b71313be18
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-05 12:42:39 -0600

    Complete score variables, calculate winner, integrate html dynamically

--- scripts.js
+++ scripts.js
@@ -2,33 +2,74 @@
 let userScore = 0;
 let computerScore = 0;
 let playerSelection = "";
-// let computerSelection = getComputerChoice();
+let computerSelection = "";
 const options = ["Rock", "Paper", "Scissors"];
-// Have computer pick either rock paper scissors
-// function getComputerChoice () {
-//     const randomIndex = Math.floor(Math.random() * options.length)
-//     const randomOption = options[randomIndex]
-//     return randomOption;
-// }
+const playerSelectionPlaceholder = document.getElementById("player-selection-result");
+const computerSelectionPlaceholder = document.getElementById("computer-selection-result");
+const userScorePlaceholder = document.getElementById("player-score");
+const computerScorePlaceholder = document.getElementById("computer-score");
+
 // Allow user to select either rock paper scissors
-function onButtonClick() {
-    if (playerSelection = "Rock") {
-        alert("You've chosen Rock!");
+document.getElementById("rockButton").addEventListener("click", function() {
+    playerSelection = "Rock";
+    getComputerChoice();
+    determineWinner();
+    displayUpdatedContent();
+});
+
+document.getElementById("paperButton").addEventListener("click", function() {
+    playerSelection = "Paper";
+    playerSelectionPlaceholder.textContent = playerSelection;
+    getComputerChoice();
+    setTimeout(function() {
+        computerSelectionPlaceholder.textContent = computerSelection;
+    }, 3000);
+});
+
+document.getElementById("scissorsButton").addEventListener("click", function() {
+    playerSelection = "Scissors";
+    playerSelectionPlaceholder.textContent = playerSelection;
+    getComputerChoice();
+    setTimeout(function() {
+        computerSelectionPlaceholder.textContent = computerSelection;
+    }, 3000);
+});
+
+// Have computer pick either rock paper scissors
+function getComputerChoice () {
+    const randomIndex = Math.floor(Math.random() * options.length)
+    const randomOption = options[randomIndex]
+    computerSelection = randomOption;
+}
+
+
+// Calculate the winner
+// Store the score
+function determineWinner() {
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        userScore++
+        computerScore++
     }
-    if (playerSelection = "Paper") {
-        alert("You've chosen Paper!");
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        computerScore++
     }
-    if (playerSelection = "Scissors") {
-        alert("You've chosen Scissors!");
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        userScore++
     }
 }
 
-document.getElementById("rockButton").addEventListener("click", onButtonClick);
-document.getElementById("paperButton").addEventListener("click", onButtonClick);
-document.getElementById("scissorsButton").addEventListener("click", onButtonClick);
-// Have computer display the users selection
-// Have computer display the computers selection
-// Calculate the winner
-// Store the score
+function displayUpdatedContent() {
+    playerSelectionPlaceholder.textContent = playerSelection;
+    setTimeout(function() {
+        computerSelectionPlaceholder.textContent = computerSelection;
+    }, 3000);
+    setTimeout(function() {
+        userScorePlaceholder.textContent += userScore.toString();
+        computerScorePlaceholder.textContent += computerScore.toString();
+    }, 5000)
+}
+
+
+
 // Display a message declaring the winner with current score
 // Display final message of winner after 5 rounds
