7e5dacff40d17bf1a9d3124fbf0519acf46cdd25
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-05 12:58:32 -0600

    Add check to endgame and reset button

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const playerSelectionPlaceholder = document.getElementById("player-selection-res
 const computerSelectionPlaceholder = document.getElementById("computer-selection-result");
 const userScorePlaceholder = document.getElementById("player-score");
 const computerScorePlaceholder = document.getElementById("computer-score");
+const endGameMessage = document.getElementById("end-game-message");
 
 // Allow user to select either rock paper scissors
 document.getElementById("rockButton").addEventListener("click", function() {
@@ -15,36 +16,33 @@ document.getElementById("rockButton").addEventListener("click", function() {
     getComputerChoice();
     determineWinner();
     displayUpdatedContent();
+    checkEndGame();
 });
 
 document.getElementById("paperButton").addEventListener("click", function() {
     playerSelection = "Paper";
-    playerSelectionPlaceholder.textContent = playerSelection;
     getComputerChoice();
-    setTimeout(function() {
-        computerSelectionPlaceholder.textContent = computerSelection;
-    }, 3000);
+    determineWinner();
+    displayUpdatedContent();
+    checkEndGame();
 });
 
 document.getElementById("scissorsButton").addEventListener("click", function() {
     playerSelection = "Scissors";
-    playerSelectionPlaceholder.textContent = playerSelection;
     getComputerChoice();
-    setTimeout(function() {
-        computerSelectionPlaceholder.textContent = computerSelection;
-    }, 3000);
+    determineWinner();
+    displayUpdatedContent();
+    checkEndGame();
 });
 
-// Have computer pick either rock paper scissors
+document.getElementById("reset-button").addEventListener("click", reset());
+
 function getComputerChoice () {
     const randomIndex = Math.floor(Math.random() * options.length)
     const randomOption = options[randomIndex]
     computerSelection = randomOption;
 }
 
-
-// Calculate the winner
-// Store the score
 function determineWinner() {
     if (playerSelection === "Rock" && computerSelection === "Rock") {
         userScore++
@@ -59,6 +57,7 @@ function determineWinner() {
 }
 
 function displayUpdatedContent() {
+    computerSelectionPlaceholder.textContent = "";
     playerSelectionPlaceholder.textContent = playerSelection;
     setTimeout(function() {
         computerSelectionPlaceholder.textContent = computerSelection;
@@ -66,10 +65,31 @@ function displayUpdatedContent() {
     setTimeout(function() {
         userScorePlaceholder.textContent += userScore.toString();
         computerScorePlaceholder.textContent += computerScore.toString();
-    }, 5000)
+    }, 5000);
 }
 
+function checkEndGame() {
+    setTimeout(function() {
+        if (userScore === 5 && computerScore === 5) {
+            endGameMessage.textContent = "It's a tie!";
+        }
+        else if (userScore === 5) {
+            endGameMessage.textContent = "You win! Congratulations!";  
+        }
+        else if (computerScore === 5) {
+            endGameMessage.textContent = "You've lost. Try again!";
+        }
+    }, 6000);
+}
 
+function reset() {
+    userScore = 0;
+    computerScore = 0;
+    playerSelectionPlaceholder.textContent = "";
+    computerSelectionPlaceholder.textContent = "";
+    userScorePlaceholder.textContent = "";
+    computerScorePlaceholder.textContent = "";
+}
 
 // Display a message declaring the winner with current score
 // Display final message of winner after 5 rounds
