ca6c899d38a5288cccc74cd9f571ec7615b2a3d2
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-05 11:06:10 -0600

    Style format, build buttons, add event listeners

--- scripts.js
+++ scripts.js
@@ -2,15 +2,30 @@
 let userScore = 0;
 let computerScore = 0;
 let playerSelection = "";
-let computerSelection = getComputerChoice();
+// let computerSelection = getComputerChoice();
 const options = ["Rock", "Paper", "Scissors"];
 // Have computer pick either rock paper scissors
-function getComputerChoice () {
-    const randomIndex = Math.floor(Math.random() * options.length)
-    const randomOption = options[randomIndex]
-    return randomOption;
-}
+// function getComputerChoice () {
+//     const randomIndex = Math.floor(Math.random() * options.length)
+//     const randomOption = options[randomIndex]
+//     return randomOption;
+// }
 // Allow user to select either rock paper scissors
+function onButtonClick() {
+    if (playerSelection = "Rock") {
+        alert("You've chosen Rock!");
+    }
+    if (playerSelection = "Paper") {
+        alert("You've chosen Paper!");
+    }
+    if (playerSelection = "Scissors") {
+        alert("You've chosen Scissors!");
+    }
+}
+
+document.getElementById("rockButton").addEventListener("click", onButtonClick);
+document.getElementById("paperButton").addEventListener("click", onButtonClick);
+document.getElementById("scissorsButton").addEventListener("click", onButtonClick);
 // Have computer display the users selection
 // Have computer display the computers selection
 // Calculate the winner
