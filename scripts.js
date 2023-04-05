6b06135030384f600552fe86fe4b5f957b06ca4a
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-05 10:33:00 -0600

    Create algorith, create computer selection, outline html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+// Create score variables to store scores
+let userScore = 0;
+let computerScore = 0;
+let playerSelection = "";
+let computerSelection = getComputerChoice();
+const options = ["Rock", "Paper", "Scissors"];
+// Have computer pick either rock paper scissors
+function getComputerChoice () {
+    const randomIndex = Math.floor(Math.random() * options.length)
+    const randomOption = options[randomIndex]
+    return randomOption;
+}
+// Allow user to select either rock paper scissors
+// Have computer display the users selection
+// Have computer display the computers selection
+// Calculate the winner
+// Store the score
+// Display a message declaring the winner with current score
+// Display final message of winner after 5 rounds
